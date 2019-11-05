23affec8cf592baca4c4392b926f8d42b3535421
Spockuto <venkythesupersaiyan@gmail.com>
2019-11-05 14:58:04 +0000

    Add New CV, Public Keys Update Visualization with Dynamic Network Graphs Added three public keys Footer updated with public keys link Resume points to 2 page CV

---
+++ js/keys.js
@@ -0,0 +1,40 @@
+// functionality to copy text from inviteCode to clipboard
+
+// trigger copy event on click
+$('#copy1, #copy2, #copy3').on('click', function(event) {
+    console.log(event);
+    copyToClipboard(event);
+  });
+  
+  // event handler
+  function copyToClipboard(e) {
+    //alert('this function was triggered');
+    // find target element
+    var
+      t = e.target, 
+      c = t.dataset.copytarget,
+      inp = (c ? document.querySelector(c) : null);
+    console.log(t);
+    console.log(inp);
+    // check if input element exist and if it's selectable
+    if (inp && inp.select) {
+      // select text
+      inp.select();
+      try {
+        // copy text
+        document.execCommand('copy');
+        inp.blur();
+  
+        // copied animation
+        t.classList.add('copied');
+        setTimeout(function() {
+          t.classList.remove('copied');
+        }, 1500);
+      } catch (err) {
+        //fallback in case exexCommand doesnt work
+        alert('please press Ctrl/Cmd+C to copy');
+      }
+  
+    }
+  
+  }
